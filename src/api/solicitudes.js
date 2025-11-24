ab5616b1896910f74f49316535b76e45144f90fb
Alejandra Moreno <aleja_5313@hotmail.com>
2025-11-23 21:57:17 -0500

    Entrega final - Prueba Tecnica Frontend

--- src/api/solicitudes.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const API = 'http://localhost:4000';
-
-export const loginApi = (data) => axios.post(`${API}/login`, data).then(r => r.data);
-export const getSolicitudes = (filters = {}) => axios.get(`${API}/solicitudes`, { params: filters }).then(r => r.data);
-export const createSolicitud = (data) => axios.post(`${API}/solicitudes`, data).then(r => r.data);
-export const updateSolicitud = (id, data) => axios.put(`${API}/solicitudes/${id}`, data).then(r => r.data);
-export const getEstadisticas = () => axios.get(`${API}/estadisticas`).then(r => r.data);
